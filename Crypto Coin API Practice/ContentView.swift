aa3f5bb89a35af5e0513f9f18680db2c669cd2b3
Antonymous1337 <holshouserantony@yahoo.com>
2024-11-12 22:32:03 -0800

    Initial Commit

---
+++ Crypto Coin API Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crypto Coin API Practice
+//
+//  Created by Antony Holshouser on 11/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crypto Coin API Practice/Crypto_Coin_API_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_Coin_API_PracticeApp.swift
+//  Crypto Coin API Practice
+//
+//  Created by Antony Holshouser on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_Coin_API_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
